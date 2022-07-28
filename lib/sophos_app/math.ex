002c77972dae427d8a82718f8cd9c5b74600bb17
Ricardo Puentes <rpuente@bancolombia.com>
2022-07-28 18:36:38 -0500

    Agrego el módulo Greeter con Patttern Matching

---
+++ lib/sophos_app/math.ex
@@ -0,0 +1,12 @@
+defmodule SophosApp.Math do
+  def sum(a,b) do
+    a + b
+  end
+end
+
+defmodule SophosApp.Geometry do
+  defmodule Rectangle do
+    def area(a), do: area(a, a)
+    def area(a, b), do: a * b
+  end
+end
