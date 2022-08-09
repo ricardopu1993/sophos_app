1383e57e8bb9d7d8bf9b04079e93117d96b0881e
Ricardo Puentes <rpuente@bancolombia.com>
2022-08-08 19:48:49 -0500

    Agrego Project Euler problema 1, dos resoluciones

--- lib/sophos_app/advent_of_code.ex
+++ lib/sophos_app/advent_of_code.ex
@@ -1,8 +1,18 @@
 defmodule SophosApp.AdventOfCode.Day1 do
+  alias SophosApp.MyList
+  import String, only: [split: 3]
+
   def deliver(instructions) do
     instructions
-    |> String.split("", trim: true)
+    |> split("", trim: true)
     |> evaluate(0)
+  end
+
+  def deliver2(instructions) do
+    instructions
+    |> split("", trim: true)
+    |> transform([])
+    |> MyList.sum()
   end
 
   defp evaluate([], floor), do: floor
@@ -13,4 +23,12 @@ defmodule SophosApp.AdventOfCode.Day1 do
 
   defp walk(floor, "("), do: floor + 1
   defp walk(floor, ")"), do: floor - 1
+
+  defp transform([], list), do: list
+  defp transform(["(" | t], list) do
+    transform(t, [1 | list])
+  end
+  defp transform([")" | t], list) do
+    transform(t, [-1 | list])
+  end
 end
