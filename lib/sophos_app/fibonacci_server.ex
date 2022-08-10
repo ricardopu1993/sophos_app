3888dc64a04c783442965f96d61f1148586a5446
Ricardo Puentes <rpuente@bancolombia.com>
2022-08-09 19:53:57 -0500

    State es ahora una tupla de 1 elemento y puede ser cualquier cosa

--- lib/sophos_app/fibonacci_server.ex
+++ lib/sophos_app/fibonacci_server.ex
@@ -2,36 +2,36 @@ defmodule SophosApp.FibonacciServer do
   alias SophosApp.Fibonacci
 
   def start() do
-    spawn(__MODULE__, :loop, [0])
+    spawn(__MODULE__, :loop, [{0}])
   end
 
   def start_link() do
-    spawn_link(__MODULE__, :loop, [0])
+    spawn_link(__MODULE__, :loop, [{0}])
   end
 
   def start_monitor() do
-    spawn_monitor(__MODULE__, :loop, [0])
+    spawn_monitor(__MODULE__, :loop, [{0}])
   end
 
-  def loop(counter) do
+  def loop({counter} = state) do
     receive do
       {:sequence, caller, n} ->
         result = Fibonacci.sequence(n)
         IO.inspect(caller)
         IO.inspect(self())
         send(caller, {:fibonacci, n, result})
-        loop(counter + 1)
+        loop({counter + 1})
 
        {:status, caller} ->
-         send(caller, {:ok, counter})
-         loop(counter)
+         send(caller, {:ok, state})
+         loop(state)
 
        {:exit, reason} ->
          IO.puts("bye for #{inspect(reason)}")
 
        message ->
          IO.inspect(message)
-         loop(counter)
+         loop(state)
          # after
         #   1500 -> IO.puts("Se acabó")
     end
